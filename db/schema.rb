de0c4d40cc1979cf5aa0a2804ab856b8825a1dad
Chhouk Titpanhara <titpanhara@gmail.com>
2019-09-25 09:30:00 +0700

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_19_111901) do
+ActiveRecord::Schema.define(version: 2019_09_19_112157) do
 
   create_table "announcements", force: :cascade do |t|
     t.datetime "published_at"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 2019_09_19_111901) do
     t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
     t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
     t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "image"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "notifications", force: :cascade do |t|
